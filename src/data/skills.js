432c8a107a3eb19201306301ee71cfb5c3c4fc92
jackhww <85800745+jackhww@users.noreply.github.com>
2024-03-10 09:52:03 +0000

    Build: (a3d93f8) Update publish.yml

--- src/data/skills.js
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-      languages: ['C', 'C++', 'Python'],
-      frameworks: ['ESP-IDF', 'RTOS', 'MicroPython', 'Arduino', 'NodeRED'],
-      platforms: ['ESP32', 'STM32', 'Raspberry Pi', 'Arduino'],
-      tools: ['VSCode', 'PlatformIO', 'Jira', 'SOLIDWORKS', 'Fusion360'],
-      others: ['IoT ', 'Robotics', 'Machine Learning', 'Computer Vision', 'CAD Design', '3D Printing', 'PCB Design', 'Git']
-    }
-  ]
